f88deab8cbf85080b6f49a3d4932a98272de4f05
Masha <marikakosareva@gmail.com>
2019-11-30 11:06:16 +0300

    logic done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -80,9 +80,108 @@ export class AppComponent {
 
   @HostListener('document:keyup', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) { 
+
+    let previousState = [[], [], [], []];
+    for (let i = 0; i < this.size; i++){
+      for (let j = 0; j < this.size; j++){
+        previousState[i][j] = this.data[i][j];
+      }
+    }
+    //let zeros = 0;
     if (event.keyCode == 38) {
       //this.newNumber();
       //this.changePosition("up");
+      //alert(event.keyCode);
+      let buf = null;
+      let buf2 = null;
+      for (let j = 0; j < this.size; j++){
+        buf = [];
+        buf2 = [];
+        for (let i = 0; i < this.size; i++){
+          buf[i] = this.data[i][j];
+        }
+        buf = buf.filter(item => item > 0);
+        //zeros += buf.filter(item => item === 0);
+        for (let k = 0; k < buf.length; k++){
+            if (buf[k] === buf[k+1]) {
+              buf2.push(buf[k]*2);
+              k++;
+              //zeros += 1;
+            }
+            else {
+              buf2.push(buf[k]);
+            }
+        }
+        for (let k = 0; k < this.size; k++){
+          if (k < buf2.length) this.data[k][j] = buf2[k];
+          else this.data[k][j] = 0;
+        }
+      }
+    }
+    else if (event.keyCode == 40) {
+        // down arrow
+        //this.changePosition("down");
+        //alert(event.keyCode);
+        let buf = null;
+        let buf2 = null;
+        for (let j = 0; j < this.size; j++){
+          buf = [];
+          buf2 = [];
+          for (let i = 0; i < this.size; i++){
+            buf[i] = this.data[this.size - i - 1][j];
+          }
+          buf = buf.filter(item => item > 0);
+          //zeros += buf.filter(item => item === 0);
+          for (let k = 0; k < buf.length; k++){
+              if (buf[k] === buf[k+1]) {
+                buf2.push(buf[k]*2);
+                k++;
+                //zeros += 1;
+              }
+              else {
+                buf2.push(buf[k]);
+              }
+          }
+          for (let k = 0; k < this.size; k++){
+            if (k < buf2.length) this.data[this.size - k - 1][j] = buf2[k];
+            else this.data[this.size - k - 1][j] = 0;
+          }
+        }
+        
+    }
+    else if (event.keyCode == 37) {
+      // left arrow
+      //this.changePosition("left");
+      //alert(event.keyCode);
+      let buf = null;
+      let buf2 = null;
+      for (let j = 0; j < this.size; j++){
+        buf = [];
+        buf2 = [];
+        for (let i = 0; i < this.size; i++){
+          buf[i] = this.data[j][i];
+        }
+        buf = buf.filter(item => item > 0);
+        //zeros += buf.filter(item => item === 0);
+        for (let k = 0; k < buf.length; k++){
+            if (buf[k] === buf[k+1]) {
+              buf2.push(buf[k]*2);
+              k++;
+              //zeros += 1;
+            }
+            else {
+              buf2.push(buf[k]);
+            }
+        }
+        for (let k = 0; k < this.size; k++){
+          if (k < buf2.length) this.data[j][k] = buf2[k];
+          else this.data[j][k] = 0;
+        }
+      }
+    }
+    else if (event.keyCode == 39) {
+      // right arrow
+      //this.changePosition("right");
       //alert(event.keyCode);
       let buf = null;
         let buf2 = null;
@@ -90,40 +189,39 @@ export class AppComponent {
           buf = [];
           buf2 = [];
           for (let i = 0; i < this.size; i++){
-            buf[i] = this.data[i][j];
+            buf[i] = this.data[j][this.size - i - 1];
           }
           buf = buf.filter(item => item > 0);
+          //zeros += buf.filter(item => item === 0);
           for (let k = 0; k < buf.length; k++){
               if (buf[k] === buf[k+1]) {
                 buf2.push(buf[k]*2);
                 k++;
+                //zeros += 1;
               }
               else {
                 buf2.push(buf[k]);
               }
           }
           for (let k = 0; k < this.size; k++){
-            if (k < buf2.length) this.data[k][j] = buf2[k];
-            else this.data[k][j] = 0;
+            if (k < buf2.length) this.data[j][this.size - k - 1] = buf2[k];
+            else this.data[j][this.size - k - 1] = 0;
           }
         }
     }
-    else if (event.keyCode == 40) {
-        // down arrow
-        //this.changePosition("down");
-        //alert(event.keyCode);
-        
+    let isStateChanged = false;
+    stateOuter: for (let i = 0; i < this.size; i++){
+      for (let j = 0; j < this.size; j++){
+        if (previousState[i][j] !== this.data[i][j]){
+          isStateChanged = true;
+          break stateOuter;
+        }
+      }
     }
-    else if (event.keyCode == 37) {
-      // left arrow
-      //this.changePosition("left");
-      //alert(event.keyCode);
-    }
-    else if (event.keyCode == 39) {
-      // right arrow
-      //this.changePosition("right");
-      //alert(event.keyCode);
-    }
+    if (isStateChanged) this.newNumber();
+    // if (!zeros){
+    //   this.newNumber();
+    // }
   }
 
   onKey(event: any) {
